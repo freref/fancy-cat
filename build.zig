7f6e6f65da70d3fd5aca155bb97ef162bde426ee
freref <35976402+freref@users.noreply.github.com>
2025-02-26 03:45:31 +0100

    feature: static linking

--- build.zig
+++ build.zig
@@ -1,25 +1,12 @@
 const std = @import("std");
 
-fn addMupdfDeps(exe: *std.Build.Step.Compile, target: std.Target) void {
-    if (target.os.tag == .macos and target.cpu.arch == .aarch64) {
-        exe.addIncludePath(.{ .cwd_relative = "/opt/homebrew/include" });
-        exe.addLibraryPath(.{ .cwd_relative = "/opt/homebrew/lib" });
-    } else if (target.os.tag == .macos and target.cpu.arch == .x86_64) {
-        exe.addIncludePath(.{ .cwd_relative = "/usr/local/include" });
-        exe.addLibraryPath(.{ .cwd_relative = "/usr/local/lib" });
-    } else if (target.os.tag == .linux) {
-        exe.addIncludePath(.{ .cwd_relative = "/home/linuxbrew/.linuxbrew/include" });
-        exe.addLibraryPath(.{ .cwd_relative = "/home/linuxbrew/.linuxbrew/lib" });
+fn addMupdfDeps(exe: *std.Build.Step.Compile, b: *std.Build, prefix: []const u8) void {
+    exe.addIncludePath(.{ .cwd_relative = b.fmt("{s}/include", .{prefix}) });
+    exe.addLibraryPath(.{ .cwd_relative = b.fmt("{s}/lib", .{prefix}) });
 
-        const linux_libs = [_][]const u8{
-            "mupdf-third", "harfbuzz",
-            "freetype",    "jbig2dec",
-            "jpeg",        "openjp2",
-            "gumbo",       "mujs",
-        };
-        for (linux_libs) |lib| exe.linkSystemLibrary(lib);
-    }
-    exe.linkSystemLibrary("mupdf");
+    exe.addObjectFile(.{ .cwd_relative = b.fmt("{s}/lib/libmupdf.a", .{prefix}) });
+    exe.addObjectFile(.{ .cwd_relative = b.fmt("{s}/lib/libmupdf-third.a", .{prefix}) });
+
     exe.linkSystemLibrary("z");
     exe.linkLibC();
 }
@@ -28,27 +15,48 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const prefix = b.option([]const u8, "prefix", "Installation prefix") orelse "./local";
+    const mupdf_build_step = b.addSystemCommand(&[_][]const u8{
+        "make",
+        "-C",
+        "thirdparty/mupdf",
+        b.fmt("prefix={s}", .{prefix}),
+        "install",
+    });
+
     const exe = b.addExecutable(.{
         .name = "fancy-cat",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
-
     exe.headerpad_max_install_names = true;
 
     const deps = .{
-        .vaxis = b.dependency("vaxis", .{ .target = target, .optimize = optimize }),
-        .fzwatch = b.dependency("fzwatch", .{ .target = target, .optimize = optimize }),
-        .fastb64z = b.dependency("fastb64z", .{ .target = target, .optimize = optimize }),
+        .vaxis = b.dependency("vaxis", .{
+            .target = target,
+            .optimize = optimize,
+        }),
+        .fzwatch = b.dependency("fzwatch", .{
+            .target = target,
+            .optimize = optimize,
+        }),
+        .fastb64z = b.dependency("fastb64z", .{
+            .target = target,
+            .optimize = optimize,
+        }),
     };
 
     exe.root_module.addImport("fastb64z", deps.fastb64z.module("fastb64z"));
     exe.root_module.addImport("vaxis", deps.vaxis.module("vaxis"));
     exe.root_module.addImport("fzwatch", deps.fzwatch.module("fzwatch"));
 
-    addMupdfDeps(exe, target.result);
+    exe.step.dependOn(&mupdf_build_step.step);
+
+    addMupdfDeps(exe, b, prefix);
+
     b.installArtifact(exe);
+    b.getInstallStep().dependOn(&mupdf_build_step.step);
 
     const run_cmd = b.addRunArtifact(exe);
     if (b.args) |args| run_cmd.addArgs(args);
