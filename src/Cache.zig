7c071d8421b686f90701ae0f7adedf969c30c48e
fdcote <48247604+fdcote@users.noreply.github.com>
2025-07-20 12:47:40 -0400

    feat: extend key for zoom and position

--- src/Cache.zig
+++ src/Cache.zig
@@ -3,7 +3,14 @@ const std = @import("std");
 const Config = @import("config/Config.zig");
 const vaxis = @import("vaxis");
 
-pub const Key = struct { colorize: bool, page: u16, width_mode: bool };
+pub const Key = struct {
+    colorize: bool,
+    page: u16,
+    width_mode: bool,
+    zoom: u32,
+    x_offset: i32,
+    y_offset: i32,
+};
 pub const CachedImage = struct { image: vaxis.Image };
 
 const Node = struct {
